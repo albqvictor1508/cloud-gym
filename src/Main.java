47fcb1dac27d211449172cdf529461f338612d79
Gilvando Alves <gilvandoalves141@gmail.com>
2024-11-28 21:36:42 -0300

    Add Main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Cria uma instância do Menu para iniciar o sistema
+        Menu menu = new Menu();
+
+        // Exibe o menu e permite que o usuário interaja com o sistema
+        menu.exibirMenu();
+    }
+}
